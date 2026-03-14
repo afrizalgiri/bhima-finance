7e84dc9e0c67f2ec81411e35eab29c5a192119d0
Bhima Finance <bhimafinance@gmail.com>
2026-03-15 04:30:43 +0700

    Fix users page: add Providers wrapper to fix infinite loading

--- app/frontend/app/users/layout.tsx
+++ app/frontend/app/users/layout.tsx
@@ -1,4 +1,5 @@
+import Providers from '../../components/Providers';
 import Layout from '../../components/Layout';
 export default function UsersLayout({ children }: { children: React.ReactNode }) {
-  return <Layout>{children}</Layout>;
+  return <Providers><Layout>{children}</Layout></Providers>;
 }
